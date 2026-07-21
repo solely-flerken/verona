764b1eb21a476a8130b51ebf99abfaca996394fa
solely-flerken <solely.flerken@proton.me>
2026-07-21 04:27:02 +0200

    feat(UX): reset scroll to top on route change

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import {BrowserRouter, Routes, Route} from 'react-router'
+import {useEffect} from 'react'
+import {BrowserRouter, Routes, Route, useLocation} from 'react-router'
 import {GatePage} from './pages/gate'
 import {ImpressumPage} from './pages/impressum'
 import {JobsPage} from './pages/jobs'
 import {LocationPage} from './pages/location'
 import {NotFoundPage} from './pages/not-found'
 
+function ScrollToTop() {
+    const {pathname} = useLocation()
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [pathname])
+    return null
+}
+
 function App() {
     return (
         <BrowserRouter>
+            <ScrollToTop/>
             <Routes>
                 <Route path="/" element={<GatePage/>}/>
                 <Route path="/impressum" element={<ImpressumPage/>}/>
