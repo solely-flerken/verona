d0a7a4861a21f373381b72f46d57c97fe440a9cc
solely-flerken <solely.flerken@proton.me>
2026-08-04 02:53:25 +0200

    chore(lint): disable no-img-element rule

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,13 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    // images.unoptimized is set in next.config.ts (required by output: 'export'),
+    // so next/image would add client JS with none of its usual benefit here.
+    rules: {
+      "@next/next/no-img-element": "off",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
